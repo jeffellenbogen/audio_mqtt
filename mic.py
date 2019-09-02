2b4b9ecaebe0e54d0416305a466e29f9cdf9ca1e
gsalaman <g.salaman@comcast.net>
2019-09-02 12:00:42 -0600

    bias now set based on total rows

--- mic.py
+++ mic.py
@@ -19,20 +19,22 @@ stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input_device_index=DEV
 sample_jump = CHUNK/64
 #sample_jump = 2
 sample_scale = 100
-sample_bias = 32 
 
 total_rows = 32
 total_columns = 32
+sample_bias = total_rows / 2 
 
 def on_message(client, userdata, message):
   global total_rows
   global total_columns
+  global sample_bias
 
   if message.topic == "display/columns":
     total_columns = int(message.payload)
     print("setting total columns to "+str(total_columns))
   elif message.topic == "display/rows":
     total_rows = int(message.payload)
+    sample_bias = total_rows / 2
     print("setting total rows to "+str(total_rows))
   else:
     print("Unknown message on display topic:"+message.topic)
